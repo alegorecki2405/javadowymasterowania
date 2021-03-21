995389ac1dfcf1dd4cfba19fd09f8481890d3634
Aleksander Gorecki <alegorecki@gmail.com>
2021-03-22 00:55:12 +0100

    some java exercises

---
+++ src/olek/gorecki/InterfaceChallenge/ISaveable.java
@@ -0,0 +1,8 @@
+package olek.gorecki.InterfaceChallenge;
+
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> list);
+}
--- src/olek/gorecki/Main.java
+++ src/olek/gorecki/Main.java
@@ -1,5 +1,6 @@
 package olek.gorecki;
 
+import olek.gorecki.InterfaceChallenge.Player;
 import olek.gorecki.LinkedListChallenge.Album;
 import olek.gorecki.LinkedListChallenge.Playlist;
 import olek.gorecki.LinkedListChallenge.Song;
@@ -9,17 +10,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Song s1 = new Song("WAP","2.44");
-        Song s2 = new Song("Chun-Li","2.55");
-        ArrayList<Song> songs = new ArrayList<>();
-        songs.add(s1);
-        songs.add(s2);
-        Album album = new Album(songs);
-        ArrayList<Album> albums = new ArrayList<>();
-        albums.add(album);
-        Playlist playlist = new Playlist(albums);
-        playlist.addSong(s1);
-        playlist.addSong(s2);
-        playlist.play();
+        Player tim = new Player("Tim",10,15);
+        System.out.println(tim);
     }
 }
